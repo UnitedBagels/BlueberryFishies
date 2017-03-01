5448041e47de2ae113db662daa7e49a71fc046b8
Thomas Art <unitedbagels@gmail.com>
2017-02-28 20:37:51 -0500

    i broke it the first time

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 import kivy
-kivy.require("1.9.0")
+
+kivy.require('1.9.0')
 
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 
-class CalcGridLayout(GridLayout):
 
-    def calculate(self, calculation):
-        if calculation:
-            try:
-                self.display.text = str(eval(calculation))
-            except Exception:
-                self.display.text = "ERR"
+# A Float layout positions and sizes objects as a percentage
+# of the window size
 
-class CalculatorApp(App):
+class FloatingApp(App):
     def build(self):
-        return CalcGridLayout()
+        return FloatLayout()
 
-calcApp = CalculatorApp()
-calcApp.run()
+flApp = FloatingApp()
+
+flApp.run()
